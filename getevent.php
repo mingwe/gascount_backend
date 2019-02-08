8e1cd417104543c9904eb9b40981071e078e5a49
pkolomiychenko <pkolomiychenko@webversor.com>
2019-02-08 19:26:13 +0200

    fixed json encode

--- getevent.php
+++ getevent.php
@@ -5,14 +5,22 @@ $tablename = 'events';
 
 
  $sql = mysqli_query($link, 'SELECT `ID`, `mileage`, `ltrs`, `date`, `isfull`, `comment` FROM `events`');
+
+ $resarr = [];
   while ($result = mysqli_fetch_array($sql)) {
-    echo json_encode($result);
+    // echo json_encode($result);
+    // print_r ($result);
+    // echo '/////////////';
+    array_push($resarr, $result);
   }
+
+  echo (json_encode($resarr));
+
 
   if (!$sql) {
     printf("Error: %s\n", mysqli_error($link));
     exit();
-}
+  }
 
 
 ?>
